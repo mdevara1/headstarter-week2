281c4e94e9f5119f49f8ad2d6c576ed21247e440
ManasaDev-1356 <mdevara1@binghamton.edu>
2024-08-04 13:06:09 -0400

    initial push

--- app/page.js
+++ app/page.js
@@ -1,113 +1,141 @@
-import Image from "next/image";
+'use client'
+import { useState, useEffect } from "react";
+import { firestore } from "@/firebase";
+import { deleteDoc, getDoc, getDocs, query, setDoc, collection, doc } from "firebase/firestore";
 
 export default function Home() {
+  const [pantry, setPantry] = useState([]);
+  const [itemName, setItemName] = useState('');
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const updatePantry = async () => {
+    const snapshot = query(collection(firestore, 'pantry'))
+    const docs = await getDocs(snapshot)
+
+    const pantryList = []
+    docs.forEach(doc => {
+      pantryList.push(
+       {
+        name: doc.id,
+        ...doc.data()
+       }
+      )
+    });
+    setPantry(pantryList)
+  }
+
+  const addItem = async () => {
+    if (itemName.trim() === '') return;
+    const docRef = doc(collection(firestore, 'pantry'), itemName);
+    const docSnap = await getDoc(docRef)
+
+    if (docSnap.exists()){
+      const { quantity } = docSnap.data()
+      await setDoc(docRef, { quantity: quantity + 1 })
+    } else {
+      await setDoc(docRef, { quantity: 1 })
+    }
+    setItemName('');
+    await updatePantry()
+  }
+
+  const increaseItemQuantity = async (item) => {
+    const docRef = doc(collection(firestore, 'pantry'), item);
+    const docSnap = await getDoc(docRef)
+
+    if (docSnap.exists()){
+      const { quantity } = docSnap.data()
+      await setDoc(docRef, { quantity: quantity + 1 })
+    }
+    await updatePantry()
+  }
+
+  const decreaseItemQuantity = async (item) => {
+    const docRef = doc(collection(firestore, 'pantry'), item);
+    const docSnap = await getDoc(docRef)
+
+    if (docSnap.exists()){
+      const { quantity } = docSnap.data()
+      if(quantity === 1){
+        await deleteDoc(docRef)
+      } else {
+        await setDoc(docRef, { quantity: quantity - 1 })
+      }
+    }
+    await updatePantry()
+  }
+
+  useEffect(() => { 
+    if (typeof window !== "undefined") {
+      updatePantry();
+    }
+  }, [])
+
+  const handleSearchChange = (e) => {
+    setSearchTerm(e.target.value);
+  }
+
+  const filteredPantry = pantry.filter(item => 
+    item.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="min-h-screen bg-gradient-to-r from-green-400 to-blue-500 p-8">
+      <div className="flex flex-col items-center mb-8">
+        <div className="flex items-center space-x-4">
+          <div className="animate-spin-slow w-16 h-16 bg-cover bg-center rounded-full" style={{ backgroundImage: `url('/logo.webp')` }}></div>
+          <h1 className="text-5xl text-white font-bold" style={{ fontFamily: "'Tangerine', serif" }}>Good Food Great Mood</h1>
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+      <div className="mb-8">
+        <input
+          type="text"
+          value={itemName}
+          onChange={(e) => setItemName(e.target.value)}
+          placeholder="Item Name"
+          className="w-full p-2 mb-2 border rounded"
         />
+        <button
+          onClick={addItem}
+          className=" p-2 bg-blue-500 text-white rounded hover:bg-blue-700"
+        >
+          Add Item
+        </button>
       </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+      <input
+        type="text"
+        value={searchTerm}
+        onChange={handleSearchChange}
+        placeholder="Search Items"
+        className="w-full p-2 mb-4 border rounded"
+      />
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      <ul className="space-y-2">
+        {filteredPantry.map((item) => (
+          <li key={item.name} className="flex justify-between items-center p-4 bg-white rounded shadow">
+            <div>
+              <p className="text-lg font-semibold">{item.name}</p>
+              <p className="text-gray-500">Quantity: {item.quantity}</p>
+            </div>
+            <div className="flex space-x-2">
+              <button
+                onClick={() => increaseItemQuantity(item.name)}
+                className="p-2 bg-green-500 text-white rounded hover:bg-green-700"
+              >
+                +
+              </button>
+              <button
+                onClick={() => decreaseItemQuantity(item.name)}
+                className="p-2 bg-red-500 text-white rounded hover:bg-red-700"
+              >
+                -
+              </button>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
---
+++ firebase.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getFirestore} from "firebase/firestore"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBjeiYjYd-vqPl01qfVTgHU8nyGXAxy-BM",
+  authDomain: "food-pantry-cbbd3.firebaseapp.com",
+  projectId: "food-pantry-cbbd3",
+  storageBucket: "food-pantry-cbbd3.appspot.com",
+  messagingSenderId: "53841348724",
+  appId: "1:53841348724:web:d6d4f4acd35c1c16572921",
+  measurementId: "G-E1KS3RX0M9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const firestore = getFirestore(app);
+
+export {firestore}
